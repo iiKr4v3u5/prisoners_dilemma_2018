bcc536e0267bfdb22b3ebc6f92d176df5fe161e5
iiKr4v3u5 <45744930+iiKr4v3u5@users.noreply.github.com>
2019-01-24 11:42:25 -1000

    update for team 9

--- 1_3_9-10_sourceFiles/team9.py
+++ 1_3_9-10_sourceFiles/team9.py
@@ -11,30 +11,14 @@ strategy_name = 'Collude but retaliate twice'
 strategy_description = 'Collude until betrayed, then betray twice. '
     
 def move(my_history, their_history, my_score, their_score):
-    ''' Arguments accepted: my_history, their_history are strings.
-    my_score, their_score are ints.
-    
-    Make my move.
-    Returns 'c' or 'b'. 
-    '''
-
-    # my_history: a string with one letter (c or b) per round that has been played with this opponent.
-    # their_history: a string of the same length as history, possibly empty. 
-    # The first round between these two players is my_history[0] and their_history[0].
-    # The most recent round is my_history[-1] and their_history[-1].
-    
-    # Analyze my_history and their_history and/or my_score and their_score.
-    # Decide whether to return 'c' or 'b'.
-    # The first round, collude
     if len(my_history) == 0:
         return 'c'
-    elif my_history[-1]== 'c' and their_history[-1]== 'b':
+    elif their_history[-1] == 'b':
         return 'b'
-    elif (len(my_history) > 1) and (my_history[-2]== 'c' and their_history[-2]== 'b'):
+    elif their_score > my_score :
         return 'b'
     else:
         return 'c'
-
     
 def test_move(my_history, their_history, my_score, their_score, result):
     '''calls move(my_history, their_history, my_score, their_score)
